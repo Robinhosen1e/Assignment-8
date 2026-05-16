e7dd1bc8e7a4416173d900d367b358fc7bfada77
Robin Hosen <nikiaafri@gmail.com>
2026-05-16 22:00:27 +0600

    added something

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -10,6 +10,10 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://assignment8-blush.vercel.app",
+  ],
   emailAndPassword: { 
     enabled: true, 
   },
